108b4c78c882e52b7e1de753f9970cf600ebd0c3
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-06 16:14:53 +0200

    add faker and seed database

--- database/seeders/PortfoliosTableSeeder.php
+++ database/seeders/PortfoliosTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Portfolio;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +14,18 @@ class PortfoliosTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 50; $i++) {
+
+            Portfolio::create([
+                'name' => $faker->words(rand(2, 10), true),
+                'client_name' => $faker->words(rand(2, 10), true),
+                'url_image' => 'https://picsum.photos/id/' . rand(1, 270) . '/500/400',
+                'pickup_date' => $faker->date(),
+                'deploy_date' => $faker->date(),
+                'description' => $faker->paragraphs(rand(2, 20), true),
+            ]);
+        }
     }
 }
