d6df93476746ae673540c48c386b2c29035ea1cf
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-07 15:42:52 +0200

    add edit template

--- app/Http/Controllers/Admin/PortfolioController.php
+++ app/Http/Controllers/Admin/PortfolioController.php
@@ -95,7 +95,7 @@ class PortfolioController extends Controller
      */
     public function edit(Portfolio $portfolio)
     {
-        //
+        return view('admin.portfolios.edit', compact('portfolio'));
     }
 
     /**
