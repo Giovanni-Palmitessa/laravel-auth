b3e7521737439aaf1a4bbb02965193ba12cfb9a9
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-07 15:25:34 +0200

    change name in create form

--- resources/views/admin/portfolios/create.blade.php
+++ resources/views/admin/portfolios/create.blade.php
@@ -7,6 +7,22 @@
         @csrf
 
         <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input
+                type="text"
+                class="form-control @error('name') is-invalid @enderror"
+                id="name"
+                name="name"
+                value="{{ old('name') }}"
+            >
+            @error('name')
+                <div class="invalid-feedback">
+                    {{ $message }} 
+                </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
             <label for="title" class="form-label">Title</label>
             <input
                 type="text"
