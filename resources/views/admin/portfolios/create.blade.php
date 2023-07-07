46d88ad943420e7078e431b99e1cb695de536aff
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-07 15:15:15 +0200

    add basic structure for create template

---
+++ resources/views/admin/portfolios/create.blade.php
@@ -0,0 +1,57 @@
+@extends('admin.layouts.base')
+
+@section('contents')
+    <h1>Add new Post</h1>
+    
+    <form method="POST" action="{{ route('admin.portfolios.store') }}">
+        @csrf
+
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input
+                type="text"
+                class="form-control @error('title') is-invalid @enderror"
+                id="title"
+                name="title"
+                value="{{ old('title') }}"
+            >
+            @error('title')
+                <div class="invalid-feedback">
+                    {{ $message }} 
+                </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="url_image" class="form-label">Image</label>
+            <input
+                type="text"
+                class="form-control @error('url_image') is-invalid @enderror"
+                id="url_image"
+                name="url_image"
+                value="{{ old('url_image') }}"
+            >
+            @error('url_image')
+                <div class="invalid-feedback">
+                    {{ $message }} 
+                </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="content" class="form-label">Content</label>
+            <textarea
+                class="form-control @error('content') is-invalid @enderror"
+                id="content"
+                rows="3"
+                name="content">{{ old('content') }}</textarea>
+            @error('content')
+                <div class="invalid-feedback">
+                    {{ $message }} 
+                </div>
+            @enderror
+        </div>
+
+        <button class="btn btn-primary">Save</button>
+    </form>
+@endsection
